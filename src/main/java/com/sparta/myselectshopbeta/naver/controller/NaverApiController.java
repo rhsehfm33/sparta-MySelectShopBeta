ffe5968a706d0d32d199ffa90afc0f68e8bad879
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2023-02-10 20:02:28 +0900

    feat: Set basic settings for front-end

--- src/main/java/com/sparta/myselectshopbeta/naver/controller/NaverApiController.java
+++ src/main/java/com/sparta/myselectshopbeta/naver/controller/NaverApiController.java
@@ -16,7 +16,7 @@ public class NaverApiController {
     private final NaverApiService naverApiService;
 
     @GetMapping("/search")
-    public List<ItemDto> searchItems(@RequestParam String query)  {
+    public List<ItemDto> searchItems(@RequestParam String query) {
         return naverApiService.searchItems(query);
     }
 }
